10b05fb882c4e717d3dcb6190efbf7c1e5af7db5
garrett griffey <garrettwgriffey@gmail.com>
2020-05-03 10:23:46 -0500

    file structure created

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+var routes = require("./controllers/burgerController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
+
