66cb8ea91d22c6a3227089075e2038027b913ecf
garrett griffey <garrettwgriffey@gmail.com>
2020-05-03 14:25:44 -0500

    seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burger;
+
+/* Insert 3 Rows into your new table */
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Hawaiian burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Avocado burger");
+
