6fe4e7b216465b397ab8cc295797e410ff2e3e21
garrett griffey <garrettwgriffey@gmail.com>
2020-05-04 18:46:14 -0500

    updated ORM

--- models/burger.js
+++ models/burger.js
@@ -3,23 +3,23 @@ var orm = require("../config/orm.js");
 // Calls the ORM functions using burger specific input for the ORM
 var burgers = {
   all: function(cb) {
-    orm.all("burgers", function(res) {
+    orm.all( function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+    orm.create( cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+    orm.update( objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+    orm.delete( condition, function(res) {
       cb(res);
     });
   }
