5ceb7a95f55a6c8ef19fc2741c62126164f58205
garrett griffey <garrettwgriffey@gmail.com>
2020-05-03 21:34:30 -0500

    burgerController revised

--- config/orm.js
+++ config/orm.js
@@ -29,6 +29,7 @@ function objToSql(ob) {
   return arr.toString();
 }
 
+// SQL statement functions
 var orm = {
   selectAll: function(cb) {
     var queryString = "SELECT * FROM burgers";
@@ -53,8 +54,23 @@ var orm = {
       if (err) throw err;
       cb(result);
     });
+  },
+  
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
   }
 };
+
 
 module.exports = orm;
 
